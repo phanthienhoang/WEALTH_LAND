7be8bf78ddfea52c804862aa5bd70f12f0da1d88
Danh Nguyen <nguyentd92@gmail.com>
2020-04-03 14:46:11 +0700

    Update Landing Page With Hottest Lands

--- Source/app/Http/Controllers/LandingPageController.php
+++ Source/app/Http/Controllers/LandingPageController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Application\Lands\Queries\GetHotestLands;
 use App\Application\News\Queries\GetLastNewsList;
 use App\Application\Projects\Queries\GetHotestProjects;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $lastestNews = GetLastNewsList::query([
             'length' => 3
         ]);
@@ -17,13 +19,16 @@ class LandingPageController extends Controller
             'length' => 6
         ]);
 
+        $hotestLands = GetHotestLands::query([
+            'length' => 6
+        ]);
+
         $compactData = [
             'lastestNews' => $lastestNews,
-            'hotestProjects' => $hotestProjects
+            'hotestProjects' => $hotestProjects,
+            'hotestLands' => $hotestLands
         ];
 
         return view('pages.landing-page.index')->with($compactData);
     }
-
-
 }
