f91bbd401953960ddfa9886889c9df68f686565f
Danh Nguyen <nguyentd92@gmail.com>
2020-04-10 00:15:38 +0700

    Style For Tabs Controls

--- Source/resources/views/pages/introduction/introduction.blade.php
+++ Source/resources/views/pages/introduction/introduction.blade.php
@@ -9,6 +9,21 @@
     <div class="page-header">
         <h1 class="page-title text-3">Giới thiệu về chúng tôi </h1><span class="page-sub-title"></span>
     </div>
+
+    <section class="control-tabs container p-0">
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <button class="btn btn-outline active">Về chúng tôi</button>
+          </div>
+          <div class="col-12 col-md-4">
+            <button class="btn btn-outline">Về chúng tôi</button>
+          </div>
+          <div class="col-12 col-md-4">
+            <button class="btn btn-outline">Về chúng tôi</button>
+          </div>
+        </div>
+      </section>
+
     <article class="container intro-details">
         <p class="intro"></p>
         <div class="title-section">
@@ -22,5 +37,5 @@
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
-    <script src="/js/contact-form.js"></script>
+    <script src="/js/introduction.js"></script>
 @endsection
