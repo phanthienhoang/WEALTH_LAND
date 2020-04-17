12defc4c6f79ed718e6e00ed0be4092ec2228249
khanhlecodegym <44794251+khanhlecodegym@users.noreply.github.com>
2020-04-17 16:13:14 +0700

    phan cap du an

--- Source/app/AppCategoryProjectLand.php
+++ Source/app/AppCategoryProjectLand.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppCategoryProjectLand extends Model
 {
-    
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'category_project_land_name_vi';
+    }
 }
