a05d44a5aa52b1e79c6d415c7c65074a80eddd5b
Danh Nguyen <nguyentd92@gmail.com>
2020-04-21 19:03:53 +0700

    Fix Logo

--- Source/resources/views/layouts/app.blade.php
+++ Source/resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
   <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-    <title>WealthLand - Bất động sản Thịnh vượng</title>
+    <title>WealthLand - Bất động sản - Wealthland.vn</title>
     <link rel="stylesheet" href="/assets/css/bootstrap-grid.min.css"/>
     
      {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
